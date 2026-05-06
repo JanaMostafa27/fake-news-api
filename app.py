a4646296c8b512af50fa91b18a9d6d4751fb5c66
Jana Mostafa <janamostafa2723@gmail.com>
2026-05-06 16:30:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -157,5 +157,4 @@ def predict():
 # ─────────────────────────────────────────────
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)t("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
